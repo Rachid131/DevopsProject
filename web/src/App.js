e4809783950ce3cde9c48a3073a72291fed356fe
Rachid131 <55383115+Rachid131@users.noreply.github.com>
2022-08-30 17:27:16 +0200

    inserted homepage

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="patch-header">
+        <Button>click me!</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
